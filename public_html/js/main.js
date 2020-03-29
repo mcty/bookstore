6644667c96d3321cd79f422d3e77750caa2fffab
Tyler M <admin@tylermacneil.com>
2020-03-28 21:52:33 -0400

    Primitive Search for title

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -4,5 +4,19 @@
  * and open the template in the editor.
  */
 function searchFunction(){
+   var input, filter, card, title, b, i, txtValue;
+   input = document.getElementById('myInput');
+   filter = input.value.toUpperCase();
+   card = document.getElementsByClassName("card");
+   
+   for(i=0;i<card.length;i++){
+       b = card[i].getElementsByClassName("title")[0];
+       txtValue = b.textContent || b.innerText;
+       if(txtValue.toUpperCase().indexOf(filter) > -1){
+           card[i].style.display="";
+       } else {
+           card[i].style.display="none";
+       }
+   }
     
 }
