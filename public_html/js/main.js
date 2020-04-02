52a2dd831d1b012ae772beacb6f4f6145c956360
Tyler M <admin@tylermacneil.com>
2020-04-02 14:41:15 -0400

    Added paulqasq fix andfinalized for merge to master

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 function searchFunction(){
    var input, filter, card, title, b, c, d, i, txtValueTitle, txtValueAuthor, txtValueTag;
    input = document.getElementById('myInput');
@@ -60,14 +59,14 @@ window.onclick = function(event) {
 function sortAZ()
 {
 	card = document.getElementsByClassName("col-sm-4");
-	for(i = 0; i < card.length; i++)
-	{
-		console.log(card[i].getElementsByClassName("title")[0].textContent || card[i].getElementsByClassName("title")[0].innerText);
-		if (card[i].style.display !== "none")
-		{
-			console.log(card[i].getElementsByClassName("title")[0].textContent || card[i].getElementsByClassName("title")[0].innerText + " is hidden");
-		}
-	}
+	//for(i = 0; i < card.length; i++)
+	//{
+		//console.log(card[i].getElementsByClassName("title")[0].textContent || card[i].getElementsByClassName("title")[0].innerText);
+		//if (card[i].style.display !== "none")
+		//{
+			//console.log(card[i].getElementsByClassName("title")[0].textContent || card[i].getElementsByClassName("title")[0].innerText + " is hidden");
+		//}
+	//}
 	
 	
 	var card, x, y, xTitle, yTitle, smallest, smallestHTML, largestHTML, smallestIndex;
@@ -78,7 +77,7 @@ function sortAZ()
 		{
 			x = card[i].getElementsByClassName("title")[0];
 			xTitle = x.textContent || x.innerText;
-			console.log(xTitle + " style = " + card[i].style.display);
+			//console.log(xTitle + " style = " + card[i].style.display);
 			smallest = xTitle;
 			smallestHTML = card[i].innerHTML;
 			smallestIndex = i;
@@ -86,10 +85,10 @@ function sortAZ()
 			{
 				if (card[j].style.display !== "none")
 				{
-					console.log("Subround " + (j) + "\n")
+					//console.log("Subround " + (j) + "\n")
 					y = card[j].getElementsByClassName("title")[0];
 					yTitle = y.textContent || y.innerText;
-					console.log("Comparing " + xTitle + " to " + yTitle);
+					//console.log("Comparing " + xTitle + " to " + yTitle);
 					if(yTitle < smallest)
 					{
 						
@@ -99,11 +98,48 @@ function sortAZ()
 					}
 				}
 			}
+			//console.log(largestHTML);
 			largestHTML = card[i].innerHTML;
 			card[i].innerHTML = smallestHTML;
 			card[smallestIndex].innerHTML = largestHTML;
 		}
 	}
+	// Get the button that opens the modal
+var btn = document.querySelectorAll("button.myBtn");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+	drop();
 	
 }
 
@@ -117,7 +153,7 @@ function sortZA()
 		{
 			x = card[i].getElementsByClassName("title")[0];
 			xTitle = x.textContent || x.innerText;
-			console.log(xTitle + " style = " + card[i].style.display);
+			//console.log(xTitle + " style = " + card[i].style.display);
 			smallest = xTitle;
 			smallestHTML = card[i].innerHTML;
 			smallestIndex = i;
@@ -125,10 +161,10 @@ function sortZA()
 			{
 				if (card[j].style.display !== "none")
 				{
-					console.log("Subround " + (j) + "\n")
+					//console.log("Subround " + (j) + "\n")
 					y = card[j].getElementsByClassName("title")[0];
 					yTitle = y.textContent || y.innerText;
-					console.log("Comparing " + xTitle + " to " + yTitle);
+					//console.log("Comparing " + xTitle + " to " + yTitle);
 					if(yTitle > smallest)
 					{
 						
@@ -143,6 +179,42 @@ function sortZA()
 			card[smallestIndex].innerHTML = largestHTML;
 		}
 	}
+	// Get the button that opens the modal
+var btn = document.querySelectorAll("button.myBtn");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+drop();
 }
 
 function sortPriceLowToHigh()
@@ -184,6 +256,43 @@ function sortPriceLowToHigh()
 			card[smallestIndex].innerHTML = largestHTML;
 		}
 	}
+	// Get the button that opens the modal
+var btn = document.querySelectorAll("button.myBtn");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+ 
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+drop();
 }
 
 function sortPriceHighToLow()
@@ -225,6 +334,42 @@ function sortPriceHighToLow()
 			card[smallestIndex].innerHTML = largestHTML;
 		}
 	}
+	// Get the button that opens the modal
+var btn = document.querySelectorAll("button.myBtn");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+drop();
 }
 
 function sortRatingHighToLow()
@@ -266,6 +411,42 @@ function sortRatingHighToLow()
 			card[smallestIndex].innerHTML = largestHTML;
 		}
 	}
+	// Get the button that opens the modal
+var btn = document.querySelectorAll("button.myBtn");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+drop();
 }
 
 function sortRatingLowToHigh()
@@ -307,6 +488,42 @@ function sortRatingLowToHigh()
 			card[smallestIndex].innerHTML = largestHTML;
 		}
 	}
+	// Get the button that opens the modal
+var btn = document.querySelectorAll("button.myBtn");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+drop();
 }
 
 
@@ -359,6 +576,42 @@ function sortAZAuthor()
 		}
 	}
 	
+	// Get the button that opens the modal
+var btn = document.querySelectorAll("button.myBtn");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+drop();
 }
 
 function sortZAAuthor()
@@ -397,6 +650,42 @@ function sortZAAuthor()
 			card[smallestIndex].innerHTML = largestHTML;
 		}
 	}
+	// Get the button that opens the modal
+var btn = document.querySelectorAll("button.myBtn");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+drop();
 }
 
 function sortDateNewest()
@@ -469,7 +758,42 @@ function sortDateNewest()
 			card[smallestIndex].innerHTML = largestHTML;
 		}
 	}
-	
+	// Get the button that opens the modal
+var btn = document.querySelectorAll("button.myBtn");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+	drop();
 }
 
 
@@ -544,4 +868,40 @@ function sortDateOldest()
 			card[smallestIndex].innerHTML = largestHTML;
 		}
 	}
+	// Get the button that opens the modal
+var btn = document.querySelectorAll("button.myBtn");
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+drop();
 }
